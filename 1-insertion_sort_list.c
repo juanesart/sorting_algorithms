1415b4f5298ab705972177d585f33a7d508bffca
juanesart <juanesartq@gmail.com>
2020-10-22 01:07:57 +0000

    insertion sort

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,42 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - insertion sort list algorthms
+ * @list: list pointer
+ * Return: No Return
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *act, *sw, *temp;
+
+	if (!list || *list == NULL)
+		return;
+	act = *list;
+	if (act->next == NULL)
+		return;
+	while (act->next != NULL)
+	{
+		sw = act->next;
+		if (act->n > sw->n)
+		{
+			temp = act;
+			while (temp != NULL && temp->n > sw->n)
+			{
+				temp->next = sw->next;
+				if (temp->next != NULL)
+					temp->next->prev = temp;
+				sw->prev = temp->prev;
+				if (sw->prev != NULL)
+					sw->prev->next = sw;
+				else
+					*list = sw;
+				temp->prev = sw;
+				sw->next = temp;
+				print_list(*list);
+				temp = sw->prev;
+			}
+			continue;
+		}
+		act = act->next;
+	}
+}
